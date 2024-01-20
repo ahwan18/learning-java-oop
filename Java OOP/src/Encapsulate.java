a1060d97502c9a6c65c7033c128e07aec2ec946f
ahwan1806 <zurin.d.arctus@gmail.com>
2024-01-20 18:34:58 +0800

    Adding attacking system and Completing the project

--- Java OOP/src/Encapsulate.java
+++ Java OOP/src/Encapsulate.java
@@ -2,12 +2,15 @@ class Client{
     private String name;
     private int baseHealth;
     private int baseAttack;
-    private Defence armor;
-    private Attack weapon;
     private int level;
     private int incrementHealth;
     private int incrementAttack;
+    private Defence armor;
+    private Attack weapon;
+    private int totalDamage;
+    private boolean isAlive;
 
+    // object member
     public Client(String name){
         this.name = name;
         this.baseHealth = 100;
@@ -15,16 +18,68 @@ class Client{
         this.level = 1;
         this.incrementHealth = 20;
         this.incrementAttack = 20;
+        this.isAlive = true;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getHealth(){
+        return this.maxHealth() - this.totalDamage;
     }
 
     public void display(){
         System.out.println("\nPlayer\t: " + this.name);
         System.out.println("Level\t: " + this.level);
-        System.out.println("Max HP\t: " + this.maxHealth());
+        System.out.println("Max HP\t: " + this.getHealth() + "/" + this.maxHealth());
         System.out.println("Power\t: " + this.getAttackPower());
+        System.out.println("Status\t: " + isAlive);
     }
 
-    public void levelUp(){
+    public void attack(Client opponent){
+        // calculate damage
+        int damage =  this.getAttackPower();
+        // print event
+        System.out.println("\n" + this.name + " is attacking "+ opponent.getName() + " with " + damage);
+        // attacking oppenent
+        opponent.defence(damage);
+
+        this.levelUp();
+    }
+
+    public void defence(int damage){
+        
+        // receive damage
+        int defencePower = this.armor.getDefencePower();
+        int deltaDamage;
+
+        System.out.println(this.name + " defence power = " + defencePower);
+        if(damage > defencePower){
+            deltaDamage = damage - defencePower;
+        } else {
+            deltaDamage = 0;
+        }
+        
+        System.out.println("damage earned = " + deltaDamage);
+        // adding total damage
+        this.totalDamage += deltaDamage;
+
+        // check is alive
+        if (this.getHealth() <= 0) {
+            this.isAlive = false;
+            this.totalDamage = this.maxHealth();
+        }
+
+        this.display();
+        
+    }
+    
+    private int getAttackPower(){
+        return this.baseAttack + this.level * this.incrementAttack + this.weapon.getAttack();
+    }
+
+    private void levelUp(){
         this.level++;
     }
 
@@ -40,9 +95,6 @@ class Client{
         return this.baseHealth + this.level * this.incrementHealth + this.armor.getAddedHealth();
     }
 
-    public int getAttackPower(){
-        return this.baseAttack + this.level * this.incrementAttack + this.weapon.getAttack();
-    }
 }
 
 class Defence{
@@ -60,6 +112,9 @@ class Defence{
         return this.strength*10 + this.health;
     }
 
+    public int getDefencePower(){
+        return this.strength*2;
+    }
     
 }
 
@@ -94,6 +149,10 @@ public class Encapsulate {
         player2.setArmor(armor2);
         player2.setWeapon(weapon2);
         player2.display();
-
+        player1.attack(player2);
+        
+        player2.attack(player1);
+        player2.attack(player1);
+        player2.attack(player1);
     }
 }
